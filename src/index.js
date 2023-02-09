34f4688e31a9310b7c81c203e1ef894876bffed2
Roberto Valdomiro <rvaldomiro@icloud.com>
2023-02-09 09:28:42 -0300

    ci: configurando coverage

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ function concatenate(s1, s2) {
     return s1.concat(' ').concat(s2)
 }
 
+function xpto1(s1, s2) {
+    return s1.concat(' ').concat(s2)
+}
+
+function xpto2(s1, s2) {
+    return s1.concat(' ').concat(s2)
+}
+
+function xpto3(s1, s2) {
+    return s1.concat(' ').concat(s2)
+}
+
 exports.concatenate = concatenate;
