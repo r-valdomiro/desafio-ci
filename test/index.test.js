7d9a520105c6239763fb2da625b2551654b8eb22
Roberto Valdomiro <rvaldomiro@icloud.com>
2023-02-09 10:55:39 -0300

    ci: configurando ci

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,13 @@
 const main = require('../src/index')
  
-test('should be concatenate two strings', () => {
+test('should concatenate two strings', () => {
     expect(main.concatenate('Roberto', 'Valdomiro')).toBe('Roberto Valdomiro');
+});
+
+test('should sum two values', () => {
+    expect(main.sum(2, 2)).toBe(4);
+});
+
+test('should subtract one value to another', () => {
+    expect(main.subtract(5, 2)).toBe(3);
 });
